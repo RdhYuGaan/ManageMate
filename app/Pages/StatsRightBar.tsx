9fab5626f47eae211d8a30fae2a720cbcdab5f13
RdhYuGaan <radithyugan22@gmail.com>
2024-10-12 23:19:35 +0530

    create header function and deploy Cricular chart

--- app/Pages/StatsRightBar.tsx
+++ app/Pages/StatsRightBar.tsx
@@ -9,11 +9,22 @@ function StatsRightBar(){
             <div className='h-[92%] w-[94%] bg-white rounded-l-3xl p-3 flex flex-col'>
                 <Header />
                 <div className='flex-col flex gap-11 items-center justify-center mt-6'>
-                    
+                     <CircularChart />
+                   
                 </div>
+
             </div>
         </div>
-    )
+    );
+
+
+    function Header(){
+        return(
+            <h2 className='text-[22px] font-bold text-center mt-7'>
+                Projects Completed...
+            </h2>
+        );
+    }
 }
 
 
