c4ffee1872a4b60f628db2d241a53670b27e14f6
RdhYuGaan <radithyugan22@gmail.com>
2024-10-18 15:37:05 +0530

    create Tab Function

--- app/Pages/AllTasks/Components/TasksList.tsx
+++ app/Pages/AllTasks/Components/TasksList.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
 
-const TasksList = () => {
+function TasksList () {
   return (
     <div className='ml-12 mt-11 flex-col flex gap-4'>
         <Tabs />
-        <div className='fle- flex-com gap-4'>
-            <Sing
+        <div className='flec- flex-col gap-4'>
+            <SingleTask />
+            <SingleTask />
+
         </div>
     </div>
-  )
+  );
 }
 
-export default TasksList
+export default TasksList;
+
+
+function Tabs(){
+  return (
+    <div className='flex items-center mt-8 ml-3 gap-6 mb-5 border'>
+      <div className='flex gap-2 bg-orange-400  font-semibold'>
+        <span>On Going Tasks</span>
+        <span className='px-2 bg-orange-400 text-white rounded-md'>7</span>
+
+      </div>
+    </div>
+
+  )
+}
